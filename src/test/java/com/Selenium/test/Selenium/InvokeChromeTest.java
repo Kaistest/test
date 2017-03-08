9b9bb20df1ee2e2a02900d14561660fdfe14a731
uid0655 <uid0655@PC0289.focus.com.tn>
2017-03-08 11:27:16 +0100

    AA

---
+++ src/test/java/com/Selenium/test/Selenium/InvokeChromeTest.java
@@ -0,0 +1,27 @@
+package com.Selenium.test.Selenium;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class InvokeChromeTest {
+
+public WebDriver driver;
+
+    public InvokeChromeTest()
+    {
+    	System.setProperty("webdriver.chrome.driver","C:\\Users\\uid0655\\Desktop\\Selenium\\Driver&doc\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+    }
+    
+    @Test
+	public void firefoxProgram() throws InterruptedException {
+
+		
+	}
+
+}
+
